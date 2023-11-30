0411d6062ac500fb769243a76a43d9242d599318
Jegatheesan Kavienan <kavienanj@gmail.com>
2023-11-30 07:22:15 +0530

    read tasks file and show optional output message

--- src/shell_runner.py
+++ src/shell_runner.py
@@ -3,14 +3,16 @@ import json
 import argparse
 from utils import shell_execute_command
 
-def run_commands(commands):
+def run_commands(commands, show_output=False):
     output, error = shell_execute_command(" && ".join(commands))
     if output:
         log = f"Executed all {len(commands)} commands successfully!\n{commands}"
-        print(f"STATUS: SUCCESS\nOUTPUT:{log}")
+        print(f"STATUS: SUCCESS\nLOG:{log}")
+        if show_output:
+            print(f"OUTPUT:\n{output}")
     if error:
         log = f"There was an error while executing the commands!, The Error:\n{error}"
-        print(f"STATUS: ERROR\nOUTPUT:{log}")
+        print(f"STATUS: ERROR\LOG:{log}")
 
 
 if __name__ == "__main__":
@@ -21,6 +23,13 @@ if __name__ == "__main__":
         required=True,
         help="List of commands to execute",
     )
+    parser.add_argument(
+        "--show-output",
+        type=bool,
+        default=False,
+        help="Show output of the commands",
+    )
     args = parser.parse_args()
     commands = args.commands
-    run_commands(commands)
+    show_output = args.show_output
+    run_commands(commands, show_output)
